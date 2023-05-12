234c664b614ecdc5574586e9b508b03770f92bd3
yuriagopian <yuri.agopian@hotmail.com>
2023-05-12 08:52:45 -0300

    chore: exception filter

---
+++ src/filters/http-exception.filter.ts
@@ -0,0 +1,37 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common';
+
+@Catch()
+export class AllExceptionsFilter implements ExceptionFilter {
+  private readonly logger = new Logger(AllExceptionsFilter.name);
+
+  catch(exception: any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+    const request = ctx.getRequest();
+
+    const statusCode =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException ? exception.getResponse() : exception;
+
+    this.logger.error(
+      `Http Status: ${statusCode} Error message: ${JSON.stringify(message)} `,
+    );
+
+    response.status(statusCode).json({
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      error: message,
+    });
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as momentTimezone from 'moment-timezone';
+import { AllExceptionsFilter } from './filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.useGlobalFilters(new AllExceptionsFilter());
+
+  /*
+    Desafio
+    Sobrescrevemos a função toJSON do Date passando um objeto moment. Deste modo 
+    quando o objeto for serializado, ele utilizará o formato de data definido por nós.
+    Todos os objetos Date serão afetados com esta implementação 
+  */
+  Date.prototype.toJSON = function (): any {
+    return momentTimezone(this)
+      .tz('America/Sao_Paulo')
+      .format('YYYY-MM-DD HH:mm:ss.SSS');
+  };
+
+  await app.listen(8080);
 }
 bootstrap();
